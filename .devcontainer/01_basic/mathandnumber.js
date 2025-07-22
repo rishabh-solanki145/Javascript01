26a2b9d0e631b966c4b1827b2c2c02d87a544a26
Rishabh Solanki <rishbh.solanki786@gmail.com>
2025-07-22 09:02:43 +0000

    math

---
+++ .devcontainer/01_basic/mathandnumber.js
@@ -0,0 +1,37 @@
+var a = "Hi i'm"
+var b = "Risbah Solanki"
+
+//console.log(b);
+//console.log(a.length);
+//var c = b.toLocaleLowerCase();
+//var c =  b.toLocaleUpperCase();
+//var c = b.includes('Risbah');
+//var c = b.indexOf("Risbah");
+
+// console.log(c);
+
+// console.log(parseInt(10.33));
+// console.log(parseFloat(10.30));
+
+ 
+console.log(Math.ceil(2.01));
+console.log(Math.floor(2.99));
+console.log(Math.pow(4,3));    //root
+console.log(Math.round(2.51));
+console.log(Math.round(2.49));
+console.log(Math.cbrt(120));    //cube root kiska hai
+console.log(Math.trunc(2.56))
+console.log(Math.sqrt(9));     //squre root kiska hai
+console.log(Math.min(8,10,12,3));
+console.log(Math.max(20,30,40,50,60));
+console.log(Math.random()*10);     
+console.log(Math.abs(5.369515));
+console.log(Math.PI);
+console.log(Math.floor(Math.random()+1));
+console.log(isFinite(255))
+
+
+
+
+
+
