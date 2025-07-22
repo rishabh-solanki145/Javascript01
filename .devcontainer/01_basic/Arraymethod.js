0a5be0d2249f96af5ee2d879972f8f9db99e7f59
Rishabh Solanki <rishbh.solanki786@gmail.com>
2025-07-22 07:45:37 +0000

    Array method

---
+++ .devcontainer/01_basic/Arraymethod.js
@@ -0,0 +1,22 @@
+//console.log("hello");
+
+var a = ["Sanjay","ram", "Risabh","karan"]
+var b = ["bajaj","tvs"];
+var c = ["chai"];
+var ages = [10,20,18,60]
+console.log(a.sort());
+console.log(a.reverse());
+console.log(a.pop());
+console.log(a.push("Rahul"));
+console.log(a.shift());
+console.log(a.unshift("ram"));
+var d = a.concat(b,c)
+console.log(a.concat(b,c))
+var e = d.join("/");
+console.log(e);
+console.log(a.slice(1,3))
+console.log(a.splice(2,1,"Neha","pawan"))
+console.log(a.indexOf("ram"))
+console.log(a.includes("ram"))
+
+
